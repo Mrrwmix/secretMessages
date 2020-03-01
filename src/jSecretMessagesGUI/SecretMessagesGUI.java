578d5e0d75fc291eb52d257fef7d821d2c3780eb
Matthew Winemiller <seidoc@gmail.com>
2020-03-01 12:55:53 -0700

    p.154

--- src/jSecretMessagesGUI/SecretMessagesGUI.java
+++ src/jSecretMessagesGUI/SecretMessagesGUI.java
@@ -15,7 +15,39 @@ public class SecretMessagesGUI extends JFrame {
 	
 	public String encode(String message, int keyVal) {
 		String output = "";
+		char key = (char)keyVal;
 		
+		for (int i = 0; i < message.length(); i++) {
+			char input = message.charAt(i);
+			if (input >= 'A' && input <= 'Z') {
+				input += key;
+				if (input > 'Z') {
+					input -= 26;
+				}
+				if (input < 'A') {
+					input += 26;
+				}
+			}
+			else if (input >= 'a' && input <= 'z') {
+				input += key;
+				if (input > 'z') {
+					input -= 26;
+				}
+				if (input < 'a') {
+					input += 26;
+				}
+			}
+			else if (input >= '0' && input <= '9') {
+				input += (keyVal % 10);
+				if (input > '9') {
+					input -= 10;
+				}
+				if (input < '0') {
+					input += 10;
+				}
+			}
+			output += input;
+		}
 		return output;
 	}
 	
@@ -40,9 +72,9 @@ public class SecretMessagesGUI extends JFrame {
 		lblNewLabel.setBounds(173, 166, 48, 14);
 		getContentPane().add(lblNewLabel);
 		
-		JButton btnNewButton = new JButton("Encode/Decode");
-		btnNewButton.setBounds(285, 162, 113, 23);
-		getContentPane().add(btnNewButton);
+		JButton btnEncodeDecode = new JButton("Encode/Decode");
+		btnEncodeDecode.setBounds(285, 162, 113, 23);
+		getContentPane().add(btnEncodeDecode);
 	}
 
 	public static void main(String[] args) {
